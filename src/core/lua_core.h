0bde0c10089f43e2d64ebd7e8c30e269b29fd400
stanzhao2 <34759974+stanzhao2@users.noreply.github.com>
2024-09-06 15:34:48 +0800

    update

--- src/core/lua_core.h
+++ src/core/lua_core.h
@@ -16,7 +16,7 @@ using namespace stdnet;
 /********************************************************************************/
 
 #define lua_success(s) (s == LUA_OK || s == LUA_YIELD)
-#define lua_yield_k(L, n, ctx, f)  lua_yieldk(L, n, (lua_KContext)ctx, f)
+#define lua_yield_k(L, n, k, f)  lua_yieldk(L, n, (lua_KContext)k, f)
 #define lua_pcall_k(L, n, r, k, f) lua_pcallk(L, n, r, 0, (lua_KContext)k, f)
 
 /********************************************************************************/
