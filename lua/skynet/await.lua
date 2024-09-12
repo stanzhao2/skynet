fe4533440c6b7e0c21e1766ec10163b841cabc7e
stanzhao2 <34759974+stanzhao2@users.noreply.github.com>
2024-09-12 14:51:53 +0800

    update

--- lua/skynet/await.lua
+++ lua/skynet/await.lua
@@ -5,7 +5,11 @@
 local function await(co, func, ...)
   local handler = bind(func, ...);
   local function callback(responser, ...)
-    responser(handler(...));
+    if responser == nil then
+      handler(...);
+    else
+      responser(handler(...));
+    end
   end
   return function(...)
     co:dispatch(bind(callback, os.responser(), ...));
